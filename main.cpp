f0380745de3a47532cfcea7576d13cddff8e0733
Yusuf Redzic <yusufredzic@gmail.com>
2023-07-05 10:36:56 -0500

    stuff 3

--- main.cpp
+++ main.cpp
@@ -1,310 +1,237 @@
 /*
- * http://ffmpeg.org/doxygen/trunk/index.html
+ * This file is part of FFmpeg.
  *
- * Main components
+ * FFmpeg is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * Format (Container) - a wrapper, providing sync, metadata and muxing for the
- * streams. Stream - a continuous stream (audio or video) of data over time.
- * Codec - defines how data are enCOded (from Frame to Packet)
- *        and DECoded (from Packet to Frame).
- * Packet - are the data (kind of slices of the stream data) to be decoded as
- * raw frames. Frame - a decoded raw frame (to be encoded or filtered).
+ * FFmpeg is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with FFmpeg; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  */
 
-#include <array>
-#include <cassert>
-#include <chrono>
-#include <cstddef>
-#include <cstdint>
-#include <cstdio>
-#include <iostream>
-#include <iterator>
+#include <cstdlib>
+#include <cstring>
 #include <memory>
-#include <thread>
-#include <unistd.h>
 
 extern "C" {
 #include <libavcodec/avcodec.h>
-#include <libavcodec/codec.h>
-#include <libavcodec/codec_par.h>
 #include <libavcodec/packet.h>
 #include <libavformat/avformat.h>
-#include <libavutil/avutil.h>
+#include <libavutil/dict.h>
 #include <libavutil/error.h>
 #include <libavutil/frame.h>
-#include <libavutil/pixfmt.h>
 }
 
-using uint8 = std::uint8_t;
-using uint16 = std::uint16_t;
-using uint32 = std::uint32_t;
-using uint64 = std::uint64_t;
+struct DecodeContext {
+    AVFormatContext* demuxer;
+    AVStream* stream;
+    AVCodecContext* decoder;
 
-using int8 = std::int8_t;
-using int16 = std::int16_t;
-using int32 = std::int32_t;
-using int64 = std::int64_t;
+    AVPacket* pkt;
+    AVFrame* frame;
 
-#define ForceInline __attribute__((always_inline)) inline
-#define NoInline __attribute__((noinline))
+    int (*process_frame)(struct DecodeContext* dc, AVFrame* frame);
+    void* opaque;
 
-namespace {
+    AVDictionary* decoder_opts;
+    int max_frames;
+};
 
-ForceInline void svprint(std::string_view strv) {
-    write(STDOUT_FILENO, strv.data(), strv.size());
-}
+int print_pixel(DecodeContext* dc, AVFrame* frame) {
+    int* numcalled = static_cast<int*>(dc->opaque);
 
-void save_gray_frame(const uint8* __restrict buf, size_t stride, size_t xsize,
-                     size_t ysize, const char* filename) {
-    auto file = std::unique_ptr<FILE, decltype([](FILE* f) { fclose(f); })>(
-        fopen(filename, "w"));
-
-    // writing the minimal required header for a pgm file format
-    // portable graymap format ->
-    // https://en.wikipedia.org/wiki/Netpbm_format#PGM_example
-
-    fprintf(file.get(), "P5\n%zu %zu\n255\n", xsize, ysize);
-
-    if (stride == xsize) {
-        auto filed = fileno(file.get());
-        auto n_written = write(filed, buf, xsize * ysize);
-        // assert (n_written > 0) && ((size_t)n_written == (xsize * ysize))
+    if (frame != nullptr) {
+        printf("data: %d\n", static_cast<int>(frame->data[0][0]));
+        *numcalled += 1;
     } else {
-        // writing line by line
-        for (size_t i = 0; i < ysize; i++) {
-            fwrite(buf, 1, xsize, file.get());
-            buf += stride;
-        }
+        printf("print_pixel() called %d times\n", *numcalled);
     }
+
+    return 0;
 }
 
-// assumes each source plane has the same dimensions
-uint32 sum_abs_diff(const uint8* src1, const uint8* src2, size_t stride,
-                    size_t width, size_t height) {
-    constexpr auto absdiff = [](uint8 a, uint8 b) {
-        return std::abs(static_cast<int32>(a) - static_cast<int32>(b));
-    };
+static int decode_read(DecodeContext* dc, int flush) {
+    const int ret_done = flush != 0 ? AVERROR_EOF : AVERROR(EAGAIN);
+    int ret = 0;
 
-    uint32 sum = 0;
-
-    if (stride == width) {
-        for (size_t i = 0; i < (width * height); i++) {
-            sum += absdiff(src1[i], src2[i]);
-        }
-    } else {
-        while ((height--) != 0) {
-            for (size_t i = 0; i < width; i++) {
-                sum += absdiff(src1[i], src2[i]);
+    while (ret >= 0 &&
+           (dc->max_frames == 0 || dc->decoder->frame_num < dc->max_frames)) {
+        ret = avcodec_receive_frame(dc->decoder, dc->frame);
+        if (ret < 0) {
+            if (ret == AVERROR_EOF) {
+                int const err = dc->process_frame(dc, nullptr);
+                if (err < 0) {
+                    return err;
+                }
             }
 
-            src1 += stride;
-            src2 += stride;
+            return (ret == ret_done) ? 0 : ret;
+        }
+
+        ret = dc->process_frame(dc, dc->frame);
+        av_frame_unref(dc->frame);
+        if (ret < 0) {
+            return ret;
+        }
+
+        if ((dc->max_frames != 0) && dc->decoder->frame_num == dc->max_frames) {
+            return 1;
         }
     }
 
-    return sum;
+    return (dc->max_frames == 0 || dc->decoder->frame_num < dc->max_frames) ? 0
+                                                                            : 1;
 }
 
-//
+int ds_run(DecodeContext* dc) {
+    int ret = 0;
 
-// bigger binary size :(
-// might have to make my own fmt library or something
-// which doesn't have too much bloat
-
-// printf that compiles to like "printf bytecode" or something would be
-// probably the most ideal. That way it's fast and has minimal calling overhead.
-// then you would run "interpreter" of bytecode.
-// and has possibility to inline prints of just a string to
-// write().
-
-} // namespace
-
-int main(int argc, const char* argv[]) {
-    auto get_time = []() { return std::chrono::high_resolution_clock::now(); };
-
-    // struct that holds some data about the container (format)
-    // does this have to be freed manually?
-
-    if (argc < 2) {
-        svprint("scenedetect-cpp requires an input file to be specified, "
-                "aborting...\n");
-        return -1;
+    ret = avcodec_open2(dc->decoder, nullptr, &dc->decoder_opts);
+    if (ret < 0) {
+        return ret;
     }
 
-    AVFormatContext* fctx = nullptr;
-
-    // avformat_open_input() handles alloc and free for us
-    if (avformat_open_input(&fctx, argv[1], nullptr, nullptr) != 0) {
-        // nonzero return value means FAILURE
-        svprint("avformat_open_input() returned failure, aborting...\n");
-        return -1;
-    }
-
-    assert(fctx);
-
-    auto fctx_owned =
-        std::unique_ptr<AVFormatContext, decltype([](AVFormatContext* fc) {
-                            avformat_free_context(fc);
-                        })>(fctx);
-
-    printf("Format %s, duration %lld us\n", fctx->iformat->long_name,
-           fctx->duration);
-
-    // this populates some fields in the context
-    // possibly not necessary for all formats
-    avformat_find_stream_info(fctx, nullptr);
-
-    printf("number of streams: %d\n", fctx->nb_streams);
-
-    uint64 time_ns = 0;
-
-    for (size_t stream_idx = 0; stream_idx < fctx->nb_streams; stream_idx++) {
-        // codec parameters for current stream
-        auto* codecpar = fctx->streams[stream_idx]->codecpar;
-
-        // find suitable decoder for the codec parameters
-        const auto* codec = avcodec_find_decoder(codecpar->codec_id);
-
-        printf("%s ", codec->name);
-
-        bool skip_decode = true;
-        if (codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
-            printf("[Video Codec] Resolution %dx%d px\n", codecpar->width,
-                   codecpar->height);
-
-            skip_decode = false;
-        } else if (codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            printf("[Audio Codec] %dCh, Sample Rate=%dhz\n",
-                   codecpar->ch_layout.nb_channels, codecpar->sample_rate);
+    while (ret >= 0) {
+        ret = av_read_frame(dc->demuxer, dc->pkt);
+        if (ret < 0) {
+            break;
         }
-
-        if (skip_decode) {
+        if (dc->pkt->stream_index != dc->stream->index) {
+            av_packet_unref(dc->pkt);
             continue;
         }
 
-        printf("\t%s, ID %d, bit_rate %lld\n", codec->long_name, (int)codec->id,
-               codecpar->bit_rate);
-
-        //  AVCodecContext is struct for decode/encode
-
-        // allocate context
-        auto* codec_ctx = avcodec_alloc_context3(codec);
-
-        // yay! this actually works as intended
-
-        auto n_threads = std::thread::hardware_concurrency();
-        if (n_threads > 0) {
-            codec_ctx->thread_count = static_cast<int32>(n_threads);
+        ret = avcodec_send_packet(dc->decoder, dc->pkt);
+        if (ret < 0) {
+            fprintf(stderr, "Error decoding: %d\n", ret);
+            return ret;
         }
+        av_packet_unref(dc->pkt);
 
-        // fill codec context with parameters
-        avcodec_parameters_to_context(codec_ctx, codecpar);
-        // initialize codec context based on AVCodec
-        avcodec_open2(codec_ctx, codec, nullptr);
-
-        // read packets from stream and decode into frames
-        // but we need to allocate the packets and frames first
-
-        std::array<AVFrame*, 2> framebuf{av_frame_alloc(), av_frame_alloc()};
-
-        auto* packet = av_packet_alloc();
-
-        // it really reads a packet basically
-        // TODO error handling
-        while (av_read_frame(fctx, packet) >= 0) {
-            if (packet->stream_index != (int)stream_idx) {
-                continue;
-            }
-
-            // send compressed packet to the CodecContext (decoder)
-            avcodec_send_packet(codec_ctx, packet);
-
-            // receive raw uncompressed frame
-            // So is this guaranteed to work this way?
-            // send one packet, receive one frame?
-
-            std::swap(framebuf[0], framebuf[1]);
-
-            // if this returns EAGAIN, we need to send more input
-
-            // here we receive uncompressed frame
-
-            // framebuf contains previous 2 frames
-            // discard old one
-
-            int response = avcodec_receive_frame(codec_ctx, framebuf[1]);
-
-            if (response == AVERROR(EAGAIN)) {
-                continue;
-            }
-
-            if (response != 0) {
-                svprint("avcodec_receive_frame(): got bad response\n");
-                return -1;
-            }
-
-            // so we are indeed decoding some frames
-            // but like some of them aren't decoding properly
-
-            // save_gray_frame(frame->data[0], frame->linesize[0], frame->width,
-            //                 frame->height, filename.c_str());
-
-            AVFrame* frame = framebuf[1];
-
-            // TODO set build options in such a way that this
-            // function call gets inlined?
-
-            printf("Frame %c (%lld) pts %lld dts %lld key_frame %d\n",
-                   av_get_picture_type_char(frame->pict_type),
-                   codec_ctx->frame_num, frame->pts, frame->pkt_dts,
-                   frame->key_frame);
-
-            // TODO manually move this if to just another loop
-            // like decode first frame separately, then do rest of decode loop
-            if (codec_ctx->frame_num > 1) {
-                // TODO assert all these strides and stuff are the same for
-                // both
-
-                auto start = get_time();
-
-                // Idea: average pixels in 8x8 blocks to avoid false
-                // positives due to noise in pixels
-
-                auto diff =
-                    sum_abs_diff(framebuf[0]->data[0], framebuf[1]->data[0],
-                                 framebuf[0]->linesize[0], framebuf[0]->width,
-                                 framebuf[0]->height);
-
-                auto end = get_time();
-
-                auto duration =
-                    std::chrono::duration_cast<std::chrono::nanoseconds>(end -
-                                                                         start);
-
-                // TODO add assert for checking if max abs diff will fit in
-                // uint32
-
-                time_ns += duration.count();
-                printf("abs_diff(): %d\n\n", diff);
-            }
-        }
-
-        for (auto&& f : framebuf) {
-            av_frame_free(&f);
-        }
-        av_packet_free(&packet);
-
-        avcodec_free_context(&codec_ctx);
-
-        for (size_t i = 0; i < 2; i++) {
-            av_frame_free(&framebuf[i]);
+        ret = decode_read(dc, 0);
+        if (ret < 0) {
+            fprintf(stderr, "Error decoding: %d\n", ret);
+            return ret;
+        } else if (ret > 0) {
+            goto finish;
         }
     }
 
-    // ok some AV1 files seem to just fail for some reason?
-    // very strange...
+    avcodec_send_packet(dc->decoder, nullptr);
+    ret = decode_read(dc, 1);
+    if (ret < 0) {
+        fprintf(stderr, "Error flushing: %d\n", ret);
+        return ret;
+    }
 
-    printf("Total time consumed: %lluns\n", time_ns);
+finish:
+    return dc->process_frame(dc, nullptr);
+}
+
+void ds_free(DecodeContext* dc) {
+    av_dict_free(&dc->decoder_opts);
+
+    av_frame_free(&dc->frame);
+    av_packet_free(&dc->pkt);
+
+    avcodec_free_context(&dc->decoder);
+    avformat_close_input(&dc->demuxer);
+}
+
+int ds_open(DecodeContext* dc, const char* url) {
+    const AVCodec* codec = nullptr;
+    int ret = 0;
+    int stream_idx = -1;
+
+    auto get_video_stream_idx = [](DecodeContext* dc) -> int {
+        for (size_t stream_idx = 0; stream_idx < dc->demuxer->nb_streams;
+             stream_idx++) {
+            if (dc->demuxer->streams[stream_idx]->codecpar->codec_type ==
+                AVMEDIA_TYPE_VIDEO) {
+                return static_cast<int>(stream_idx);
+            }
+        }
+        return -1;
+    };
+
+    memset(dc, 0, sizeof(*dc));
+
+    dc->pkt = av_packet_alloc();
+    dc->frame = av_frame_alloc();
+    if ((dc->pkt == nullptr) || (dc->frame == nullptr)) {
+        ret = AVERROR(ENOMEM);
+        goto fail;
+    }
+
+    ret = avformat_open_input(&dc->demuxer, url, nullptr, nullptr);
+    if (ret < 0) {
+        fprintf(stderr, "Error opening input file: %d\n", ret);
+        return ret;
+    }
+
+    // I think this is necessary for it to set the fields and shit
+    avformat_find_stream_info(dc->demuxer, nullptr);
+
+    stream_idx = get_video_stream_idx(dc);
+    if (stream_idx < 0) {
+        return AVERROR_DECODER_NOT_FOUND;
+    }
+
+    dc->stream = dc->demuxer->streams[stream_idx];
+
+    codec = avcodec_find_decoder(dc->stream->codecpar->codec_id);
+    if (codec == nullptr) {
+        return AVERROR_DECODER_NOT_FOUND;
+    }
+
+    dc->decoder = avcodec_alloc_context3(codec);
+    if (dc->decoder == nullptr) {
+        return AVERROR(ENOMEM);
+    }
+
+    // set to 0 for automatic thread conut detection
+    dc->decoder->thread_count = 0;
+
+    ret = avcodec_parameters_to_context(dc->decoder, dc->stream->codecpar);
+    if (ret < 0) {
+        goto fail;
+    }
 
     return 0;
+
+fail:
+    ds_free(dc);
+    return ret;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("usage: scenedetect-cpp <video_file>\n");
+        return -1;
+    }
+
+    DecodeContext dc{};
+
+    // uhh... error checking maybe?
+    int ret = ds_open(&dc, argv[1]);
+    if (ret < 0) {
+        printf("error occurred!\n");
+        ds_free(&dc);
+        return -1;
+    }
+
+    auto frame_count = std::make_unique<int>(0);
+    dc.process_frame = print_pixel;
+    dc.opaque = (int*)frame_count.get();
+    dc.max_frames = 0;
+    ds_run(&dc);
+
+    ds_free(&dc);
 }
